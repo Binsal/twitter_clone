1414062de50b244a4e3da04eeb696777ed3c2378
Binsal <96328531+Binsal@users.noreply.github.com>
2023-11-05 11:07:13 +0530

    added routes

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/login'   element={<Login/>}/>
+      </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { Module } from "module";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5RJzMCfRcXToTHCtqkmGGxByiyD1QQ9M",
+  authDomain: "twitter-f9fe2.firebaseapp.com",
+  projectId: "twitter-f9fe2",
+  storageBucket: "twitter-f9fe2.appspot.com",
+  messagingSenderId: "270726246375",
+  appId: "1:270726246375:web:39ecf0fc63e90575879742"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
