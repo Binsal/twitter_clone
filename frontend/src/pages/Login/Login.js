41fb77112fe2b4b4b5186f13226dfbb832a30980
Binsal <96328531+Binsal@users.noreply.github.com>
2023-11-05 15:17:52 +0530

    changed styling and code login and signup

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -3,13 +3,18 @@ import twitterImage from "../../assests/images/twitter_login.jpg"
 import TwitterIcon from '@mui/icons-material/Twitter';
 import auth from '../../firebase.init';
 import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
-
+import "./Login.css"
+import {GoogleButton} from 'react-google-button';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from "react-router-dom";
 
 const Login = () =>{
 
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     //const [errorMessage,setError] = useState('');
+    const navigate = useNavigate();
+
     const [
         signInWithEmailAndPassword,
         user,
@@ -17,31 +22,77 @@ const Login = () =>{
         error,
       ] = useSignInWithEmailAndPassword(auth);
 
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if(user || googleUser){
+        navigate('/')
+        console.log(user)
+        console.log(googleUser);
+      }
+
+      if(error){
+        console.log(error.message)
+      }
+
+      if(loading){
+        console.log("loading....")
+      }
+
     const handleSubmit = e =>{
         e.preventDefault();
         console.log(user);
         signInWithEmailAndPassword(email,password);
     }
 
+    const handleGoogleSignIn = () =>{
+        signInWithGoogle();
+    }
+
+
     return(
         <div className="login-container">
            <div className="image-container">
-            <img src={twitterImage} alt="twitter banner"></img>
+            <img className="image" src={twitterImage} alt="twitter banner"></img>
            </div>
            <div className="form-container">
-            <TwitterIcon/>
-            <h2>Happening Now</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="email" className="email" 
-                    placeholder="Email address" onChange={(e)=>setEmail(e.target.value)}/>
-                <input type="password" className="password" 
-                    placeholder="Password"
-                    onChange={(e)=>setPassword(e.target.value)}
-                    />
-                <div className="btn-login">
-                    <button type="submit" className="btn">Login</button>
-                </div>
-            </form>
+           <div className="form-box">
+                <TwitterIcon style={{color:'skyblue'}}/>
+                    <h2 className="heading">Happening Now</h2>
+                    <h3 className="heading1">What happening today</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" className="email" 
+                            placeholder="Email address" onChange={(e)=>setEmail(e.target.value)}/>
+                        <input type="password" className="password" 
+                            placeholder="Password"
+                            onChange={(e)=>setPassword(e.target.value)}
+                            />
+                        <div className="btn-login">
+                            <button type="submit" className="btn">Login</button>
+                        </div>
+                    </form>
+           </div>
+            <hr/>
+                    <div className="google-button">
+                        <GoogleButton
+                            className='g-btn'
+                            type='light'
+                            onClick={handleGoogleSignIn}
+                        />
+                    </div>
+                    <div>
+                        Don't have account?
+                        <link
+                            to='/signup'
+                            style={{
+                                textDecoration:'none',
+                                color:'skyblue',
+                                fontWeight:'600',
+
+                            }}
+                        >
+                        </link>
+                        Sign up
+                    </div>
            </div>
         </div>
     );
--- frontend/src/pages/Login/Signup.js
+++ frontend/src/pages/Login/Signup.js
@@ -3,6 +3,9 @@ import twitterImage from "../../assests/images/twitter_login.jpg"
 import TwitterIcon from '@mui/icons-material/Twitter';
 import auth from '../../firebase.init';
 import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import {GoogleButton} from 'react-google-button';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from "react-router-dom";
 
 const Signup = () =>{
     const [username,setUsername] = useState('');
@@ -10,6 +13,9 @@ const Signup = () =>{
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     //const [errorMessage,setError] = useState('');
+
+    const navigate = useNavigate();
+
     const [
         createUserWithEmailAndPassword,
         user,
@@ -17,7 +23,21 @@ const Signup = () =>{
         error,
       ] = useCreateUserWithEmailAndPassword(auth);
 
-      console.log(user);
+      const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+      if(user || googleUser){
+        navigate('/')
+        console.log(user)
+        console.log(googleUser);
+      }
+
+      if(error){
+        console.log(error.message)
+      }
+
+      if(loading){
+        console.log("loading....")
+      }
       
     const handleSubmit = e =>{
         e.preventDefault();
@@ -25,33 +45,61 @@ const Signup = () =>{
         createUserWithEmailAndPassword(email,password);
     }
 
+    const handleGoogleSignIn = () =>{
+        signInWithGoogle();
+    }
+
 
     return (
-        <div className="signup-container">
+        <div className="login-container">
            <div className="image-container">
-            <img src={twitterImage} alt="twitter banner"></img>
+            <img className="image" src={twitterImage} alt="twitter banner"></img>
            </div>
            <div className="form-container">
-            <TwitterIcon/>
-            <h2>Happening Now</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" className='display-name' placeholder="username"
-                    onChange={(e)=>setUsername(e.target.value)}
-                />
-                <input type="text" className="display-name"
-                    placeholder="Enter full name"
-                    onChange={(e)=>setName(e.target.value)}
-                 />
-                <input type="email" className="email" 
-                    placeholder="Email address" onChange={(e)=>setEmail(e.target.value)}/>
-                <input type="password" className="password" 
-                    placeholder="Password"
-                    onChange={(e)=>setPassword(e.target.value)}
+           <div className="form-box">
+                <TwitterIcon className='Twittericon' style={{color:'skyblue'}} />
+                <h2 className="heading">Happening Now</h2>
+                <h3 className="heading1">Join twitter today</h3>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className='display-name' placeholder="username"
+                        onChange={(e)=>setUsername(e.target.value)}
                     />
-                <div className="btn-login">
-                    <button type="submit" className="btn">Sign up</button>
+                    <input type="text" className="display-name"
+                        placeholder="Enter full name"
+                        onChange={(e)=>setName(e.target.value)}
+                    />
+                    <input type="email" className="email" 
+                        placeholder="Email address" onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="password" className="password" 
+                        placeholder="Password"
+                        onChange={(e)=>setPassword(e.target.value)}
+                        />
+                    <div className="btn-login">
+                        <button type="submit" className="btn">Sign up</button>
+                    </div>
+                </form>
+                <hr/>
+                    <div className="google-button">
+                        <GoogleButton
+                            className='g-btn'
+                            type='light'
+                            onClick={handleGoogleSignIn}
+                        />
+                    </div>
+                    <div>
+                        Already have an account?
+                        <link
+                        to='/login'
+                        style={{
+                            textDecoration:'none',
+                            color:'skyblue',
+                            fontWeight:'600',
+
+                        }}
+                        >
+                        </link>
+                    </div>
                 </div>
-            </form>
            </div>
         </div>
     )
